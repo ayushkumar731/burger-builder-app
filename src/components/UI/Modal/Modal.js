c60a26c75dbd63bcd25f90570ed2fe89eee38566
Ayush Kumar <kumarayush731@gmail.com>
2020-10-13 14:24:54 +0530

    backdrops comonents implimented

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,16 @@
  import React from 'react';
  import classes from './Modal.module.css';
+ import Aux from '../../../hoc/Auxilliary';
+ import Backdrop from '../Backdrop/Backdrop';
 
  const modal =(props) =>(
-    <div className={classes.Modal} style={{
-       transform:props.show?'translateY(0)':'transalteY(-180vh)',
-       display:props.show?'block':'none'}}
-    >{props.children}</div>
+    <Aux>
+       <Backdrop show={props.show} clicked={props.modelClosed}/>
+      <div className={classes.Modal} style={{
+         transform:props.show?'translateY(0)':'transalteY(-180vh)',
+         display:props.show?'block':'none'}}
+      >{props.children}</div>
+    </Aux>
  )
 
  export default modal;
