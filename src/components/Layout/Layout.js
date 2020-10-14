6327b05303cecfc1599a5eadc8b2ecb012549321
Ayush Kumar <kumarayush731@gmail.com>
2020-10-14 08:57:54 +0530

    nav bar added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxilliary';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 const layout =(props)=>(
     <Auxiliary>
-        <div> Toolbar ,Sidebar, Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content}>{props.children}</main>
     </Auxiliary>
 )
