65ebf5855338af7727911ab5c1da59372a3ed933
Ayush Kumar <kumarayush731@gmail.com>
2020-10-14 14:36:53 +0530

    improve app and structuring folder

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,30 @@
- import React from 'react';
+ import React,{Component} from 'react';
  import classes from './Modal.module.css';
- import Aux from '../../../hoc/Auxilliary';
+ import Aux from '../../../hoc/Auxilliary/Auxilliary';
  import Backdrop from '../Backdrop/Backdrop';
 
- const modal =(props) =>(
-    <Aux>
-       <Backdrop show={props.show} clicked={props.modelClosed}/>
-      <div className={classes.Modal} style={{
-         transform:props.show?'translateY(0)':'transalteY(-180vh)',
-         display:props.show?'block':'none'}}
-      >{props.children}</div>
-    </Aux>
- )
+ class Modal extends Component {
 
- export default modal;
+   shouldComponentUpdate(nextProps,nextState) {
+      return nextProps.show!==this.props.show;
+   }
+
+   componentDidUpdate() {
+      console.log('models updated')
+   }
+
+
+    render(){
+       return(
+            <Aux>
+               <Backdrop show={this.props.show} clicked={this.props.modelClosed}/>
+               <div className={classes.Modal} style={{
+                  transform:this.props.show?'translateY(0)':'transalteY(-180vh)',
+                  display:this.props.show?'block':'none'}}
+               >{this.props.children}</div>
+            </Aux>
+       )
+    }
+ }
+
+ export default Modal;
