e0e8a868a9c9bdb626f0cb064d7afc8b40908dbd
Ayush Kumar <kumarayush731@gmail.com>
2020-10-20 11:45:51 +0530

    setting up routing and routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {Route,Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div >
       <Layout>
-        <BurgerBuilder/>
-        <Checkout/>
+        <Switch>
+            <Route path="/" component={BurgerBuilder}/>
+            <Route path="/checkout" component={Checkout}/>
+        </Switch>
       </Layout>
     </div>
   );
--- src/components/Order/Checkout/CheckoutSummary.js
+++ src/components/Order/Checkout/CheckoutSummary.js
@@ -8,7 +8,7 @@ const checkOutSummary=(props)=>{
     <div className={classes.CheckoutSummary}>
         <h1>We hope it tastes well!</h1>
         <div style={{width:'100%',margin:'auto'}}>
-        <Burger ingredients={props.ingredients}/>
+            <Burger ingredients={props.ingredients}/>
         </div>
         <Button btnType='Danger' clicked>Cancel</Button>
         <Button btnType='Success' clicked>Continue</Button>
