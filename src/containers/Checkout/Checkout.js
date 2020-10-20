1f8a03b0ccd4b565b2a822ade781c41e8444596d
Ayush Kumar <kumarayush731@gmail.com>
2020-10-20 13:07:43 +0530

    navigation back and to next page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,10 +12,21 @@ class Checkout extends Component {
         }
     }
 
+    checkeoutCancelledHandler=() => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler=() => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render(){
         return(
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary 
+                ingredients={this.state.ingredients}
+                checkeoutCancelled={this.checkeoutCancelledHandler}
+                checkoutContinued={this.checkoutContinuedHandler}/>
             </div>
         )
     }
