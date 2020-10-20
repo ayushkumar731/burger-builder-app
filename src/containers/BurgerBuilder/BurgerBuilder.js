864141db715df72ea7a0affe75f136e41e917fea
Ayush Kumar <kumarayush731@gmail.com>
2020-10-20 13:30:21 +0530

    passing ingredients via qyery

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -129,7 +129,17 @@ class BurgerBuilder extends Component{
         //             }
         //         })
         //     });
-        this.props.history.push('/checkout');
+        const queryParams =[];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i)+ '=' + encodeURIComponent(this.state.ingredients[i]))
+        }
+        console.log(queryParams);
+        const queryString =queryParams.join('&');
+
+        this.props.history.push({
+            pathname:'/checkout',
+            search:'?'+queryString
+        });
     }
 
     render(){ 
